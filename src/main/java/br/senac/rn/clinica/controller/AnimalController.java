6d741250348e9d127b56f9efda33035b1d3d2012
Weskley Bezerra <weskleymb@hotmail.com>
2023-10-31 21:23:46 -0300

    primeiro commit

--- src/main/java/br/senac/rn/clinica/controller/AnimalController.java
+++ src/main/java/br/senac/rn/clinica/controller/AnimalController.java
@@ -3,9 +3,7 @@ package br.senac.rn.clinica.controller;
 import br.senac.rn.clinica.model.Animal;
 import br.senac.rn.clinica.repository.AnimalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +17,11 @@ public class AnimalController {
     @GetMapping
     public List<Animal> listaTodos() {
         return repository.findAll();
+    }
+
+    @PostMapping
+    public void salvar(@RequestBody Animal animal) {
+        repository.save(animal);
     }
 
 }
